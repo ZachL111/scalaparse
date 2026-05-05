f3bcdaa487eaaf4b3ed6d01f170fcb64c39c010f
ZachL111 <dog3miner@gmail.com>
2026-05-05 13:32:10 -0700

    Initial commit

---
+++ tests/Test.scala
@@ -0,0 +1,13 @@
+object TestRunner {
+  def main(args: Array[String]): Unit = {
+    val signalcase_1 = Signal(56, 89, 14, 20, 7)
+    assert(Policy.score(signalcase_1) == 74)
+    assert(Policy.classify(signalcase_1) == "review")
+    val signalcase_2 = Signal(59, 81, 15, 20, 12)
+    assert(Policy.score(signalcase_2) == 94)
+    assert(Policy.classify(signalcase_2) == "review")
+    val signalcase_3 = Signal(103, 87, 13, 6, 10)
+    assert(Policy.score(signalcase_3) == 268)
+    assert(Policy.classify(signalcase_3) == "accept")
+  }
+}
